e3407ee5200f75cc09776eb1aae2b3b6eef5696a
XianYu <72987911+ceeyu@users.noreply.github.com>
2023-07-19 15:24:19 +0800

    Create search_firstname_service.dart

---
+++ lib/Not3_Http測試/Post相關/search_firstname/search_firstname_service.dart
@@ -0,0 +1,120 @@
+import 'package:flutter_project2/models/post_test.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _enterfirstnamecontroller =
+      TextEditingController();
+
+  //final TextEditingController _chatdatacontroller = TextEditingController();
+  Future<List<dynamic>>? _futureLogin;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Gardeners Search Name (Post) Post'),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(8),
+        child: (_futureLogin == null) ? buildColumn() : buildFutureBuilder(),
+      ),
+    );
+  }
+
+  Column buildColumn() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        SizedBox(
+          width: 300,
+          height: 80,
+          child: TextField(
+            controller: _enterfirstnamecontroller,
+            decoration: const InputDecoration(
+              hintText: 'Enter Account for searching',
+              labelText: "搜尋詞",
+              prefixIcon: Icon(Icons.person),
+            ),
+          ),
+        ),
+        /*
+        SizedBox(
+          width: 300,
+          height: 80,
+          child: TextField(
+            controller: _recieverpasswordcontroller,
+            //obscureText: true,
+            decoration: const InputDecoration(
+              hintText: 'Enter Recever Account',
+              labelText: "接收者帳號",
+              prefixIcon: Icon(Icons.lock),
+            ),
+          ),
+        ),
+        
+        SizedBox(
+          width: 300,
+          height: 80,
+          child: TextField(
+            controller: _chatdatacontroller,
+            //obscureText: true,
+            decoration: const InputDecoration(
+              hintText: 'Enter chat data',
+              labelText: "傳送訊息",
+              prefixIcon: Icon(Icons.lock),
+            ),
+          ),
+        ),
+        */
+        SizedBox(
+          width: 150,
+          height: 48,
+          child: ElevatedButton(
+            onPressed: () {
+              setState(() {
+                _futureLogin = createLogin(_enterfirstnamecontroller.text
+                    );
+              });
+            },
+            child: const Text('Show'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  FutureBuilder<List<dynamic>> buildFutureBuilder() {
+    return FutureBuilder<List<dynamic>>(
+      future: _futureLogin,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasData) {
+          return ListView.builder(
+            padding: const EdgeInsets.all(20),
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              final item = snapshot.data![index];
+              return ListTile(
+                
+                title: Text('first_name : ${item['first_name']}'),
+                //subtitle: Text(  'send_time:${item['send_time']}' 'chat_data :${item['chat_data']}'),
+              );
+            },
+          );
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        } else {
+          return const Text('No data available');
+        }
+      },
+    );
+  }
+}
