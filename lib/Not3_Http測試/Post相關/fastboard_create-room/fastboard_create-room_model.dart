6a9b3679bbc204023035200a98182714b576f16b
XianYu <72987911+ceeyu@users.noreply.github.com>
2023-07-24 14:45:14 +0800

    Create fastboard_create-room_model.dart

---
+++ lib/Not3_Http測試/Post相關/fastboard_create-room/fastboard_create-room_model.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'dart:developer';
+// import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+class LoginModel {
+  final String uuid;
+  final String roomToken;
+  final String appIdentifier;
+
+  const LoginModel(
+      {required this.roomToken,
+      required this.appIdentifier,
+      required this.uuid});
+  factory LoginModel.fromJson(Map<String, dynamic> json) {
+    return LoginModel(
+      //API return
+      //roomData: json['roomData'],
+      uuid: json['roomData']['uuid'],
+      roomToken: json['roomToken'],
+      appIdentifier: json['appIdentifier'],
+    );
+  }
+}
+
+Future<List<dynamic>> createLogin(String region) async {
+  final response = await http.post(
+    Uri.parse('http://120.126.16.222/fastboards/create-room'),
+    headers: <String, String>{'Content-Type': 'application/json'},
+    body: jsonEncode(<String, String>{
+      //input
+      'region': region,
+      //'receiver_account': receiver_account,
+    }),
+  );
+  if (response.statusCode >= 200 && response.statusCode < 300) {
+    List<dynamic> body = jsonDecode(response.body);
+    return body;
+  } else {
+    log(response.body);
+    log('${response.statusCode}');
+    throw Exception('${response.reasonPhrase},${response.statusCode}');
+  }
+}
