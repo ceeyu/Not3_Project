838c05b65b19fbfb68ff84ac1b90521b5818a123
XianYu <72987911+ceeyu@users.noreply.github.com>
2023-07-25 13:43:53 +0800

    Update main.dart

--- lib/Not3_Http測試/筆記相關/Agora_create-room/lib/main.dart
+++ lib/Not3_Http測試/筆記相關/Agora_create-room/lib/main.dart
@@ -1,82 +1,17 @@
-/*
+
 import 'package:flutter/material.dart';
-import 'agora_service.dart';
-import 'custom_layout.dart';
-import 'page.dart';
-import 'quick_start.dart';
-
-final List<FastExamplePage> _allPages = <FastExamplePage>[
-  const QuickStartPage(),
-  const CustomLayoutPage(),
-  
-];
-
-void main() {
-  const bool isProduction = bool.fromEnvironment('dart.vm.product');
-  if (isProduction) {
-    debugPrint = (String? message, {int? wrapWidth}) => null;
-  }
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('FFastboard'),
-          ),
-          body: ListView.builder(
-            itemCount: _allPages.length,
-            itemBuilder: (appContext, index) => ListTile(
-              leading: _allPages[index].leading,
-              title: Text(_allPages[index].title),
-              onTap: () => _pushPage(appContext, _allPages[index]),
-            ),
-          )
-          // body: _allPages[0],
-          ),
-    );
-  }
-
-  void _pushPage(BuildContext context, FastExamplePage page) {
-    Navigator.of(context).push(MaterialPageRoute<void>(
-        builder: (_) => Scaffold(
-              body: page,
-              // prevent resize, when the keyboard appears
-              resizeToAvoidBottomInset: false,
-            )));
-  }
-}
-*/
-
-// /*
-import 'package:flutter/material.dart';
-//import 'package:fastboard_flutter_example/agora_service.dart';
 
 import 'custom_layout.dart';
 import 'page.dart';
 import 'quick_start.dart';
 
 final List<FastExamplePage> _allPages = <FastExamplePage>[
-  const QuickStartPage(),
-  const CustomLayoutPage(),
+  const QuickStartPage(), //快速開始頁面
+  const CustomLayoutPage(), //自定義佈局
 ];
 
 void main() {
+  // 檢查是否在生產環境，若是則禁用debugPrint輸出
   const bool isProduction = bool.fromEnvironment('dart.vm.product');
   if (isProduction) {
     debugPrint = (String? message, {int? wrapWidth}) => null;
@@ -107,9 +42,9 @@ class _MyAppState extends State<MyApp> {
         body: ListView.builder(
           itemCount: _allPages.length,
           itemBuilder: (appContext, index) => ListTile(
-            leading: _allPages[index].leading,
-            title: Text(_allPages[index].title),
-            onTap: () => _pushPage(appContext, _allPages[index]),
+            leading: _allPages[index].leading,  // 列表項目前面的圖示
+            title: Text(_allPages[index].title), // 列表項目的標題文字
+            onTap: () => _pushPage(appContext, _allPages[index]), //點擊事件
           ),
         ),
       ),
@@ -117,13 +52,13 @@ class _MyAppState extends State<MyApp> {
   }
 
   void _pushPage(BuildContext context, FastExamplePage page) {
+    // 將選定的頁面推送到新的路由中進行跳轉
     Navigator.of(context).push(MaterialPageRoute<void>(
       builder: (_) => Scaffold(
-        body: page,
-        // prevent resize, when the keyboard appears
+        body: page, // 頁面內容由FastExamplePage的子類來決定
+        // 防止鍵盤彈出時造成頁面大小變化
         resizeToAvoidBottomInset: false,
       ),
     ));
   }
 }
-// */
